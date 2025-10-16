62784c2fcd943eda1fdc56c0b21aed846a96ff2e
john18prince2007-stack <john18prince.2007@gmail.com>
2025-10-16 12:44:47 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: '𝑪𝑱.', // Your name
-  ownerNumber: '2347048130686', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: '𝐋.', // Your name
+  ownerNumber: '2347083025356', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 

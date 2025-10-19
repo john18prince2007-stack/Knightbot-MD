5055b73cf723481a5fa289a09531211d31d8221c
john18prince2007-stack <john18prince.2007@gmail.com>
2025-10-19 20:22:28 +0100

    Update botName in settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
   packname: '𝐋. 𝐁𝐎𝐓',
   author: '𝐋.‎',
-  botName: "𝐋. 𝐁𝐎𝐓",
+  botName: "𝐋.'s 𝐁𝐎𝐓",
   botOwner: '𝐋.', // Your name
   ownerNumber: '2347083025356', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',

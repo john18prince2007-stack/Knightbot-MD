0e6772e10f58f9567e1d5f0208e3a6ae3f98d192
john18prince2007-stack <john18prince.2007@gmail.com>
2025-11-05 05:18:14 +0100

    Update bot settings and metadata

--- settings.js
+++ settings.js
@@ -1,14 +1,14 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: '𝑷𝒓𝒊𝒏𝒄𝒆 ⛓ 𝒄𝒇𝒙.',
+  author: '‎𝑷𝒓𝒊𝒏𝒄𝒆 ⛓',
+  botName: "𝒄𝒇𝒙 Bot",
+  botOwner: '𝑷𝒓𝒊𝒏𝒄𝒆 ⛓ 𝒄𝒇𝒙.', // Your name
+  ownerNumber: '2347083025356', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "The Aura 😈",
   version: "3.0.0",
   updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
 };
